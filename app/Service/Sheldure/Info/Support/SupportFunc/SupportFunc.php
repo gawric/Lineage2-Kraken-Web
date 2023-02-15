83a5218374e760a37f76c7ba5df069ec74e1e307
gawric <gawric@mail.ru>
2023-02-15 14:16:13 +0500

    Рефактор: Разбил SheldureServers на маленькие классы. Запросы на  два отдельных запроса!

---
+++ app/Service/Sheldure/Info/Support/SupportFunc/SupportFunc.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service\Sheldure\Info\Support\SupportFunc;
+
+use Config;
+use App\Service\Status\StatusServer;
+use App\Service\Status\Support\SupportFuncStatus;
+use App\Models\InfoServer;
+use App\Models\CharactersStatic;
+use App\Service\Sheldure\ISheldure;
+use App\Service\Sheldure\Info\Support\GeneralFilters;
+use App\Service\Sheldure\Info\Support\SqlFilter\ClanDataByIdFilter;
+use App\Models\Server\ServerCharacters;
+use App\Models\Server\ServerClanData;
+use Illuminate\Support\Collection;
+
+
+    class SupportFunc
+    {
+     
+        public function convertIdClanToNameClan($allModelCharacters ,  $result_clan){
+            foreach($allModelCharacters as $model){
+                $model->clan = $this->getClanName($result_clan , $model->clan);
+                info($model); 
+            }
+
+        }
+
+        public function createModel($server_id , $resultArr){
+            $temp = [];
+            if(isset($resultArr)){
+                $listclassid = Config::get('lineage2.class_id.list_class_id');
+                foreach($resultArr as $valueArr){
+                    array_push($temp , new CharactersStatic($server_id , $listclassid , $valueArr));
+                }
+            }
+           return $temp;
+        } 
+
+
+
+        public function getClanName($result_clan , $model_clan_id){
+                if(isset($result_clan)){
+                    return $this->searchId($result_clan , $model_clan_id);
+                }
+                return "Non";
+        }
+
+        public function searchId($result_clan , $model_clan_id){
+
+            $name = "Non";
+            foreach($result_clan as $currentClanId){
+
+                $clan_id_server = (int) $currentClanId['clan_id'];
+
+                if(!is_null($model_clan_id)){
+                    if($clan_id_server == $model_clan_id){
+                        $name = $currentClanId['clan_name'];
+                    }
+                }
+      
+            }
+           return $name;
+        }
+
+    }
+
+?>
